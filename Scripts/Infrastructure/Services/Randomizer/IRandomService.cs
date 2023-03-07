c2df67c7a692c6cd7c58f58fab03bdb73bfb6bd8
rumartov <58605808+rumartov@users.noreply.github.com>
2023-03-07 15:42:51 +0300

    Add random service

---
+++ Scripts/Infrastructure/Services/Randomizer/IRandomService.cs
@@ -0,0 +1,9 @@
+﻿using CodeBase.Infrastructure.Services;
+
+namespace CodeBase.Services.Randomizer
+{
+  public interface IRandomService : IService
+  {
+    int Next(int minValue, int maxValue);
+  }
+}
